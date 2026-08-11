d17b818204c0945525d5c49adcff778a2d9f3a29
241210041 <241210041@nitdelhi.ac.in>
2026-08-11 22:21:01 +0530

    important changes

--- chat-app/server/middleware/authMiddleware.js
+++ chat-app/server/middleware/authMiddleware.js
@@ -1,22 +1,33 @@
 const authMiddleware = (req, res, next) => {
+  try {
     const authHeader = req.headers.authorization;
 
-      if (!authHeader) {
-        return res.status(401).json({
-            success: false,
-            message: "Authentication required"
-        });
+    if (!authHeader) {
+      return res.status(401).json({
+        success: false,
+        message: "Authentication required",
+      });
     }
-     const token = authHeader.split(" ")[1];
-      if (!token) {
-        return res.status(401).json({
-            success: false,
-            message: "Authentication token missing"
-        });
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Authentication token missing",
+      });
     }
-     console.log(token);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+
+    
+    console.log(token);
     console.log("Authentication Middleware Running");
     next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
+    });
+  }
 };
-
 module.exports = authMiddleware;
